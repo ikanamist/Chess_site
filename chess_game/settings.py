c4efa14410d46e34c7a5352b11f1e7981a3f4259
ikanamist <ikanamist@gmail.com>
2024-08-31 23:02:23 +0200

    Initial commit

--- chess_game/settings.py
+++ chess_game/settings.py
@@ -15,8 +15,6 @@ from pathlib import Path
 import dj_database_url
 import django_heroku
 
-# Активируем Django-Heroku.
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,3 +130,6 @@ LOGOUT_REDIRECT_URL = "/"
 
 # Настройки Whitenoise
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+# Активируем Django-Heroku.
+django_heroku.settings(locals())
